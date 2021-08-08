97d1a53a6ae3113dafcf09e4bef27089bc518e27
Kaustav Ghosh <teetangh@gmail.com>
2021-08-09 00:48:56 +0530

    updated the pages

--- notified-frontend/src/App.js
+++ notified-frontend/src/App.js
@@ -2,11 +2,52 @@ import './App.css';
 import Navbar from "./components/Navbar.js";
 import SocialMediaSideBar from './components/SocialMediaSideBar';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <SocialMediaSideBar/>
+      <Router>
+        <Navbar />
+        <SocialMediaSideBar />
+
+        <Switch>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/blogs">
+            <Blogs />
+          </Route>
+
+          <Route path="/events">
+            <Events />
+          </Route>
+
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+          <Route path="/signin">
+            <Signin />
+          </Route>
+
+          <Route path="/signout">
+            <Signout />
+          </Route>
+
+        </Switch>
+
+
+      </Router>
     </div>
   );
 }
