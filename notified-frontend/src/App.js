3a65769de9d58d52b19f2585f5df78c52bb188d0
Kaustav Ghosh <teetangh@gmail.com>
2021-08-08 23:54:33 +0530

    added Navbar and pages

--- notified-frontend/src/App.js
+++ notified-frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <h1>Notified</h1>
     </div>
   );
 }
